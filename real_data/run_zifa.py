3d7975e8c9d9bdbf57ad0f2a3b299166680773cd
Davide Risso <risso.davide@gmail.com>
2016-10-17 11:25:58 -0700

    Added full ZIFA algorithm

--- real_data/run_zifa.py
+++ real_data/run_zifa.py
@@ -2,11 +2,12 @@
 
 from argparse import ArgumentParser
 from pandas import read_csv
-from ZIFA import block_ZIFA
+from ZIFA import ZIFA,block_ZIFA
 import numpy as np
 
 def main():
-    parser = ArgumentParser(description="Fit a ZIFA model on the data (with the block algorithm).")
+    parser = ArgumentParser(description="Fit a ZIFA model on the data.")
+    parser.add_argument('-b', '--block', action='store_true', default=False, help="Whether the block algorithm should be used.")
     parser.add_argument('input_file', type=str, help="The input CSV file.")
     parser.add_argument('output_file', type=str, help="The output CSV file.")
 
@@ -18,7 +19,10 @@ def main():
     lc = np.array(df)
     Y = np.transpose(lc)
 
-    Z, model_params  = block_ZIFA.fitModel(Y, 2)
+    if(args.block):
+        Z, model_params  = block_ZIFA.fitModel(Y, 2)
+    else:
+        Z, model_params  = ZIFA.fitModel(Y, 2)
 
     np.savetxt(args.output_file, Z, delimiter=',')
 
