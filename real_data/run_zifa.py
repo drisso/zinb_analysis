c4d23722866575de932d23008b2e1bca0320af6c
Davide Risso <dar2062@med.cornell.edu>
2017-02-22 16:22:36 -0500

    ZIFA: Added parameter to wrapper to specify number of dims

--- real_data/run_zifa.py
+++ real_data/run_zifa.py
@@ -11,6 +11,7 @@ import numpy as np
 def main():
     parser = ArgumentParser(description="Fit a ZIFA model on the data.")
     parser.add_argument('-b', '--block', action='store_true', default=False, help="Whether the block algorithm should be used.")
+    parser.add_argument('-d', '--dimensions', type=int, default=2, help="The number of dimensions [2].")
     parser.add_argument('input_file', type=str, help="The input CSV file.")
     parser.add_argument('output_file', type=str, help="The output CSV file.")
 
@@ -23,9 +24,9 @@ def main():
     Y = np.transpose(lc)
 
     if(args.block):
-        Z, model_params  = block_ZIFA.fitModel(Y, 2)
+        Z, model_params  = block_ZIFA.fitModel(Y, args.dimensions)
     else:
-        Z, model_params  = ZIFA.fitModel(Y, 2)
+        Z, model_params  = ZIFA.fitModel(Y, args.dimensions)
 
     np.savetxt(args.output_file, Z, delimiter=',')
 
