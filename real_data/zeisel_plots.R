cccf70a789b7781cae19bb3e097b97d7a4d19513
Davide Risso <dar2062@med.cornell.edu>
2017-02-22 16:22:58 -0500

    New plots for zeisel

--- real_data/zeisel_plots.R
+++ real_data/zeisel_plots.R
@@ -42,13 +42,16 @@ col2 <- brewer.pal(8, "Set2")
 colMerged <- col2[level1]
 
 data.frame(Dim1=pc_tc[,1], Dim2=pc_tc[,2]) %>%
-  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point() -> panel1_pca
+  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point() +
+  scale_color_brewer(palette="Set2") -> panel1_pca
 
 data.frame(Dim1=zifa_tc[,1], Dim2=zifa_tc[,2]) %>%
-  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point() -> panel1_zifa
+  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point() +
+  scale_color_brewer(palette="Set2") -> panel1_zifa
 
 data.frame(Dim1=zinb@W[,1], Dim2=zinb@W[,2]) %>%
-  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point()  -> panel1_zinb
+  ggplot(aes(Dim1, Dim2, colour=level1)) + geom_point()  +
+  scale_color_brewer(palette="Set2")  -> panel1_zinb
 
 p1 <- plot_grid(panel1_pca + theme(legend.position = "none"),
           panel1_zifa + theme(legend.position = "none"),
@@ -81,26 +84,62 @@ save_plot("zeisel_fig1.pdf", fig1,
           base_aspect_ratio = 1.3
 )
 
-library(cluster)
-# d <- dist(zinb@W)
-# s <- silhouette(as.numeric(level1), d)
-# mean(s[,3])
-# tapply(s[,3], s[,1], mean)
-# plot(s, border=col2[sort(as.numeric(level1), decreasing = TRUE)], main="ZINB")
-#
-# d <- dist(pc_tc[,1:2])
-# s <- silhouette(as.numeric(level1), d)
-# mean(s[,3])
-# tapply(s[,3], s[,1], mean)
-# plot(s, border=col2[sort(as.numeric(level1), decreasing = TRUE)], main="PCA")
-#
-# d <- dist(zifa_tc)
-# s <- silhouette(as.numeric(level1), d)
-# mean(s[,3])
-# tapply(s[,3], s[,1], mean)
-# plot(s, border=col2[sort(as.numeric(level1), decreasing = TRUE)], main="ZIFA")
+qc <- data.frame(age, coverage, detection_rate, diameter, nmolecule, well)
+qc <- apply(qc, 2, as.numeric)
 
-methods <- list(pc_raw[,1:2], pc_tc[,1:2], pc_tmm[,1:2], pc_fq[,1:2],
+cors <- lapply(1:3, function(i) abs(cor(pc_tc[,i], qc, method="spearman")))
+cors <- unlist(cors)
+bars <- data.frame(AbsoluteCorrelation=cors,
+                   QC=rep(stringr::str_to_lower(colnames(qc)), 3),
+                   Dimension=as.factor(rep(1:3, each=ncol(qc))))
+
+bars %>%
+  ggplot(aes(Dimension, AbsoluteCorrelation, group=QC, fill=QC)) +
+  geom_bar(stat="identity", position='dodge') +
+  scale_fill_manual(values=col2) + ylim(0, 1) -> panel2_pca
+
+cors <- lapply(1:3, function(i) abs(cor(zifa_tc[,i], qc)))
+cors <- unlist(cors)
+bars <- data.frame(AbsoluteCorrelation=cors,
+                   QC=rep(stringr::str_to_lower(colnames(qc)), 3),
+                   Dimension=as.factor(rep(1:3, each=ncol(qc))))
+
+bars %>%
+  ggplot(aes(Dimension, AbsoluteCorrelation, group=QC, fill=QC)) +
+  geom_bar(stat="identity", position='dodge') +
+  scale_fill_manual(values=col2) + ylim(0, 1) -> panel2_zifa
+
+cors <- lapply(1:3, function(i) abs(cor(zinb@W[,i], qc)))
+cors <- unlist(cors)
+bars <- data.frame(AbsoluteCorrelation=cors,
+                   QC=rep(stringr::str_to_lower(colnames(qc)), 3),
+                   Dimension=as.factor(rep(1:3, each=ncol(qc))))
+
+bars %>%
+  ggplot(aes(Dimension, AbsoluteCorrelation, group=QC, fill=QC)) +
+  geom_bar(stat="identity", position='dodge') +
+  scale_fill_manual(values=col2) + ylim(0, 1) -> panel2_zinb
+
+p2 <- plot_grid(panel2_pca + theme(legend.position = "none"),
+                panel2_zifa + theme(legend.position = "none"),
+                panel2_zinb + theme(legend.position = "none"),
+                labels=c("D", "E", "F"), align = "h", ncol=3)
+
+legend2 <- get_legend(panel2_pca)
+lower <- plot_grid(p2, legend2, rel_widths = c(3, 1))
+
+fig1bis <- plot_grid(upper, lower, ncol=1, nrow=2)
+
+save_plot("zeisel_fig1bis.pdf", fig1bis,
+          ncol = 3,
+          nrow = 3,
+          base_aspect_ratio = 1.3
+)
+
+
+library(cluster)
+
+methods <- list(pc_raw[,1:3], pc_tc[,1:3], pc_tmm[,1:3], pc_fq[,1:3],
                 zifa_raw, zifa_tc, zifa_tmm, zifa_fq,
                 zinb@W)
 names(methods) <- c(paste0("PCA_", c("RAW", "TC", "TMM", "FQ")),
@@ -114,14 +153,20 @@ sil_cl <- sapply(seq_along(methods), function(i) {
   mean(ss[,3])
 })
 
-pdf("zeisel_sil.pdf")
-barplot(sil_cl, col=col1[met_type], horiz = TRUE, names.arg = names(methods), las=2, main="Average silhouette width")
-dev.off()
+bars <- data.frame(AverageSilhouette=sil_cl, Method=names(methods), Type=met_type)
 
-cors <- sapply(seq_along(methods), function(i) {
-  max(abs(cor(methods[[i]][,1], detection_rate)), abs(cor(methods[[i]][,2], detection_rate)))
-})
+bars %>%
+  ggplot(aes(Method, AverageSilhouette, group=Type, fill=Type)) +
+  geom_bar(stat="identity", position='dodge') +
+  scale_fill_manual(values=col1) + coord_flip() +
+  theme(legend.position = "none") -> sil
 
-pdf("zeisel_sil.pdf")
-barplot(cors, col=col1[met_type], horiz = TRUE, names.arg = names(methods), las=2, main="Absolute correlation with Detection Rate")
-dev.off()
+sil2 <- plot_grid(sil, NULL, NULL, ncol=3, nrow=1, labels="G")
+fig1_tris <- plot_grid(upper, lower, sil2, ncol=1, nrow=3)
+fig1_tris
+
+save_plot("zeisel_fig1tris.pdf", fig1_tris,
+          ncol = 3,
+          nrow = 3,
+          base_aspect_ratio = 1.3
+)
