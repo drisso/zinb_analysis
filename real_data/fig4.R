c0f0f6bce9e0a2c16c83de4f41c3cd80de180385
Davide Risso <dar2062@med.cornell.edu>
2017-09-20 17:17:36 -0400

    Changes to figure 4

--- real_data/fig4.R
+++ real_data/fig4.R
@@ -43,6 +43,8 @@ df1 %>%
   ggplot(aes(x = Dim1, y = Dim2, color = Cluster)) +
   geom_point(size = 0.5) + scale_color_manual(values=pal) -> fig_tsne_zinb
 
+save_plot("zinb_tsne_pbmc.png", fig_tsne_zinb, base_aspect_ratio = 1.5, base_height = 8)
+
 df1 %>%
   ggplot(aes(x = W1, y = W2, color = Cluster)) +
   geom_point(size = 0.5) + scale_color_manual(values=pal) -> fig_W
@@ -79,3 +81,114 @@ df3 %>%
   ggplot(aes(x = Dim1, y = Dim2, color = Cluster)) +
   geom_point(size = 0.5) + scale_color_manual(values=pal) -> fig_tsne_zinb_seq
 save_plot("Sup_pbmc_seq.png", fig_tsne_zinb_seq, base_aspect_ratio = 1.5, base_height = 8)
+
+## heatmap of marker genes
+pipestance_path <- "tenx_pbmc"
+pbmc <- cellrangerRkit::load_cellranger_matrix(pipestance_path)
+# use_genes <- cellrangerRkit::get_nonzero_genes(pbmc)
+# epbmc <- as.matrix(exprs(pbmc[use_genes,]))
+
+rownames(dense) <- fData(pbmc)[rownames(dense), 2]
+rownames(tc) <- fData(pbmc)[rownames(tc), 2]
+
+se <- SummarizedExperiment(dense)
+zw <- zinbwave(se, fitted_model = zinb_10)
+
+norm <- assay(zw, "normalizedValues")
+means <- t(apply(norm, 1, tapply, z_cl, mean))
+means2 <- t(apply(tc, 1, tapply, z_cl, mean))
+
+genes <- c("CD3D", "CD8A", "NKG7", "CD79A", "CD14", #"S100A9", "LYZ",
+           "FCGR3A", #"AIF1", "FTL", "LST1",
+           "CD1C", #"HLA.DR", "GZMB",
+           "SERPINF1", #"ITM2C",
+           "PF4")
+gg <- genes[which(genes %in% rownames(means))]
+gg2 <- genes[which(genes %in% rownames(means2))]
+
+library(pheatmap)
+cols <- colorRampPalette(rev(brewer.pal(11, "RdBu")))(100)
+pdf("pbmc68k_heatmap.pdf")
+pheatmap(log1p(means2)[gg2,], cluster_cols = FALSE, cluster_rows = FALSE, scale="row", color = cols)
+dev.off()
+
+## Silhouette width (ZINB)
+W <- getW(zinb_10)
+centers <- apply(W, 2, tapply, z_cl, mean)
+f <- function(i) {
+  d <- sapply(levels(z_cl), function(c) {
+    sqrt(sum((W[i,] - centers[c,])^2))
+  })
+  ai <- d[as.character(z_cl[i])]
+  bi <- min(d[names(d) != as.character(z_cl[i])])
+  s <- (bi - ai)/max(ai, bi)
+  return(as.numeric(s))
+}
+
+s <- sapply(seq_len(nrow(W)), f)
+mean(s)
+tapply(s, z_cl, mean)
+
+## Silhouette width (PCA)
+library(Seurat)
+pbmc.data <- Read10X("tenx_pbmc/outs/filtered_gene_bc_matrices/hg19/")
+pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 1, min.genes = 1,
+                           project = "10X_PBMC")
+
+## Build SNN
+k.param = 10
+k.scale = 10
+n.cells = NCOL(pbmc.data)
+data.use <- pc_tc[,1:10]
+
+my.knn <- FNN::get.knn(as.matrix(data.use), k = min(k.scale * k.param, n.cells - 1))
+nn.ranked <- cbind(1:n.cells, my.knn$nn.index[, 1:(k.param-1)])
+nn.large <- my.knn$nn.index
+
+w <- Seurat:::CalcSNNSparse(cell.names = colnames(pbmc.data),
+                            k.param = k.param,
+                            nn.large = nn.large,
+                            nn.ranked = nn.ranked,
+                            prune.SNN = 1/15,
+                            print.output = FALSE)
+
+pbmc@snn <- w
+
+## Run modularity clustering
+resolution <- 0.6
+pbmc <- Seurat:::RunModularityClustering(object = pbmc, SNN = w,
+                                         modularity = 1, resolution = resolution,
+                                         algorithm = 1, n.start = 100,
+                                         n.iter = 10, random.seed = 0,
+                                         print.output = FALSE, temp.file.location = NULL)
+pbmc <- Seurat:::GroupSingletons(pbmc, pbmc@snn)
+name <- paste("res.", resolution, sep = "")
+pbmc <- StashIdent(pbmc, name)
+
+pca2_cl <- pbmc@ident
+names(pca2_cl) <- paste0(names(pbmc@ident), "-1")
+
+table(pca2_cl, pca_cl)
+table(pca2_cl, z_cl)
+
+P <- pc_tc[,1:10]
+centers <- apply(P, 2, tapply, pca_cl, mean)
+g <- function(i) {
+  d <- sapply(levels(pca_cl), function(c) {
+    sqrt(sum((P[i,] - centers[c,])^2))
+  })
+  ai <- d[as.character(pca_cl[i])]
+  bi <- min(d[names(d) != as.character(pca_cl[i])])
+  s <- (bi - ai)/max(ai, bi)
+  return(as.numeric(s))
+}
+
+sp <- sapply(seq_len(nrow(P)), g)
+mean(sp)
+tapply(sp, pca_cl, mean)
+
+means3 <- t(apply(tc, 1, tapply, pca_cl, mean))
+pheatmap(log1p(means3)[gg2,], cluster_cols = FALSE, cluster_rows = FALSE, scale="row", color = cols)
+
+means4 <- t(apply(tc, 1, tapply, pca2_cl, mean))
+pheatmap(log1p(means4)[gg2,], cluster_cols = FALSE, cluster_rows = FALSE, scale="row", color = cols)
--- real_data/oe_plots_slingshot.R
+++ real_data/oe_plots_slingshot.R
@@ -31,18 +31,103 @@ pdf("oe_slingshot_zinbwave.pdf")
 pairs(lineages, type="lineages", col = pal[cl], show.constraints = TRUE, pch=19)
 dev.off()
 
+pdf("oe_slingshot_zinbwave_2d.pdf")
+plot(X[,c(1, 2)], pch=19, col=pal[cl], xlab="Dim1", ylab="Dim2")
+lines(lineages, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
+
+plot(X[,c(1, 2)], pch=19, col=pal[cl], xlab="Dim1", ylab="Dim2")
+lines(lineages, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+panel1 <- recordPlot()
 
 ## PCA
-their_cl <- factor(colData(seObj)$publishedClusters)
-cl2 <- droplevels(their_cl[!their_cl %in% c("-2")])
+load("oeHBCdiff_finalClusterObject.Rda")
+cl2 <- factor(primaryClusterNamed(cmobj2), levels=paste0("m", c(1:5, 7:12, 14, 15)))
 pal2 <- c("#1B9E77", "antiquewhite2", "cyan", "#E7298A",
               "#A6CEE3", "#666666", "#E6AB02", "#FFED6F", "darkorchid2",
               "#B3DE69", "#FF7F00", "#A6761D", "#1F78B4")
 
-X2 <- W[!their_cl %in% c("-2"),]
-X2 <- cmdscale(dist(X2), k = 3)
-plot(X2, pch=19, col=pal2[cl2])
+pc <- prcomp(t(log1p(assay(cmobj2))))
+X2 <- pc$x[,1:5]
+lineages2 <- getLineages(X2, clusterLabels = cl2, start.clus = "m1", end.clus = c("m12", "m4", "m15"))
 
-lineages2 <- getLineages(X2, clusterLabels = cl2, start.clus = "1", end.clus = "")
+pdf("oe_slingshot_pca.pdf")
 pairs(lineages2, type="lineages", col = pal2[cl2], show.constraints = TRUE, pch=19)
+dev.off()
 lineages2
+
+pdf("oe_slingshot_pca_2d.pdf")
+plot(X2[,c(1, 2)], pch=19, col=pal2[cl2], xlab="Dim1", ylab="Dim2")
+lines(lineages2, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
+
+plot(X2[,c(1, 2)], pch=19, col=pal2[cl2], xlab="Dim1", ylab="Dim2")
+lines(lineages2, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+panel3 <- recordPlot()
+
+## no constraints
+lineages3 <- getLineages(X2, clusterLabels = cl2, start.clus = "m1")
+
+pdf("oe_slingshot_pca_noc.pdf")
+pairs(lineages3, type="lineages", col = pal2[cl2], show.constraints = TRUE, pch=19)
+dev.off()
+lineages3
+
+pdf("oe_slingshot_pca_2d_noc.pdf")
+plot(X2[,c(1, 2)], pch=19, col=pal2[cl2], xlab="Dim1", ylab="Dim2")
+lines(lineages3, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
+
+plot(X2[,c(1, 2)], pch=19, col=pal2[cl2], xlab="Dim1", ylab="Dim2")
+lines(lineages3, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+panel2 <- recordPlot()
+
+## no constraints -- zinbwave clusters
+names(cl) <- rownames(X)
+names(cl2) <- rownames(X2)
+idx <- intersect(names(cl), names(cl2))
+cl3 <- cl[idx]
+X3 <- X2[idx,]
+lineages4 <- getLineages(X3, clusterLabels = cl3, start.clus = "c1")
+
+pdf("oe_slingshot_pca_noc_zinbcl.pdf")
+pairs(lineages4, type="lineages", col = pal[cl3], show.constraints = TRUE, pch=19)
+dev.off()
+lineages4
+
+pdf("oe_slingshot_pca_2d_noc_zinbcl.pdf")
+plot(X3[,c(1, 2)], pch=19, col=pal[cl3], xlab="Dim1", ylab="Dim2")
+lines(lineages4, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
+
+## 3-d
+X4 <- pc$x[,1:3]
+lineages5 <- getLineages(X4, clusterLabels = cl2, start.clus = "m1", end.clus = c("m12", "m4", "m15"))
+
+pdf("oe_slingshot_pca3.pdf")
+pairs(lineages5, type="lineages", col = pal2[cl2], show.constraints = TRUE, pch=19)
+dev.off()
+lineages5
+
+pdf("oe_slingshot_pca3_2d.pdf")
+plot(X4[,c(1, 2)], pch=19, col=pal2[cl2], xlab="Dim1", ylab="Dim2")
+lines(lineages5, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
+
+## ZINB-WaVE (5 components)
+X5 <- W[!our_cl %in% c("-1", "c4"), ]
+X5 <- cmdscale(dist(X5), k = 5)
+
+plot(X5, pch=19, col=pal[cl])
+
+lineages6 <- getLineages(X5, clusterLabels = cl, start.clus = "c1")
+
+pdf("oe_slingshot_zinbwave5.pdf")
+pairs(lineages6, type="lineages", col = pal[cl], show.constraints = TRUE, pch=19)
+dev.off()
+lineages6
+
+pdf("oe_slingshot_zinbwave5_2d.pdf")
+plot(X[,c(1, 2)], pch=19, col=pal[cl], xlab="Dim1", ylab="Dim2")
+lines(lineages6, type="lineages", show.constraints = TRUE, pch=19, dims = 1:2)
+dev.off()
